c8bc9c708a7b8e7e700818ee2794f7381c4fb351
Alex <alex_mccarrick@hotmail.com>
2021-02-22 17:12:03 +0000

    adds map with event pins

--- my-app/api/ticketmaster.js
+++ my-app/api/ticketmaster.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 export const getFirst15Events = () => { 
-    return axios.get('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=leeds&size=15&apikey=v1DQXdduptbOblISnoeBRMNvW5eBDgfj')
+    return axios.get('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=manchester&size=15&apikey=v1DQXdduptbOblISnoeBRMNvW5eBDgfj')
     .then(({data}) => {
         return data._embedded.events
     })
     .then((events) => {
         return events.map((event) => {
             return { name: event.name, date: event.dates.start.localDate, time: event.dates.start.localTime, 
-            venue: `${event._embedded.venues[0].name}, ${event._embedded.venues[0].city.name}`, postCode: event._embedded.venues[0].postalCode, location: event._embedded.venues[0].location }
+            venue: `${event._embedded.venues[0].name}, ${event._embedded.venues[0].city.name}`, postCode: event._embedded.venues[0].postalCode, location: event._embedded.venues[0].location, id: event.id }
             }
         )}
     )
--- my-app/screens/EventList.js
+++ my-app/screens/EventList.js
@@ -1,31 +1,81 @@
 import * as api from '../api/ticketmaster';
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { Text, View, StyleSheet, ScrollView, StatusBar, SafeAreaView } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default class EventList extends Component {
 
     state = {
-        events: []
+        events: [],
+        initialLatitude: 53.48,
+        initialLongitude: -2.24
     }
 
     componentDidMount() {
         
        api.getFirst15Events().then((events) => {
-            this.setState({events})
+            this.setState({events, initialLatitude: +events[0].location.latitude, initialLongitude: +events[0].location.longitude})
         })
     }
 
     render() {
 
-        const { events } = this.state;
+        const { events, initialLatitude, initialLongitude } = this.state;
 
         return (
-            <View>
+            <SafeAreaView style={styles.page}>
+            <View style={styles.header}><Text>{"\n"}{"\n"}{"\t"}Header goes here! NC-Proj</Text></View>
+            <MapView style={styles.map} 
+            initialRegion={{
+            latitude: initialLatitude,
+            longitude: initialLongitude,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05
+            }}>
                 {events.map((event) => {
-                    return <Text>Event: {event.name}, Date & Time: {event.date} {event.time}, 
-                            Venue: {event.venue}, Post Code: {event.postCode}</Text>
+                    return <Marker key={event.id} coordinate={{latitude: +event.location.latitude, longitude: +event.location.longitude}}/>
                 })}
-            </View>
+            </MapView>
+            <ScrollView style={styles.container}>
+                {events.map((event) => {
+                    return <View key={event.id} style={styles.eventText}>
+                                <Text>
+                                  <Text style={styles.eventName}>{event.name}</Text> {"\n"}
+                                  <Text style={styles.eventDate}>Date: {event.date} {event.time}</Text> {"\n"}
+                                  Venue: {event.venue} {"\n"}Post Code: {event.postCode}
+                                </Text>
+                            </View>
+                })}
+            </ScrollView>
+            </SafeAreaView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    header: {
+        flex: 0.2,
+        backgroundColor: 'pink'
+    },
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+        backgroundColor: 'pink',
+    },
+    map: {
+        flex: 1,
+    },
+    eventText: {
+        marginBottom: 25,
+        borderColor: "red",
+        borderStyle: "solid",
+        borderWidth: 1,
+        flex: 1,
+        width: '100%'
+    },
+    eventName: {
+        fontWeight: "bold",
+        fontSize: 20
+    },
+
+})
