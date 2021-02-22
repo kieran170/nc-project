5e5880d4dce5cbf3defd7256c6aee27a420d5cf1
kieran cookson <kieran170@hotmail.co.uk>
2021-02-22 14:02:56 +0000

    started signin/Up

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignInUp from "./components/SignInUp";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Team!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={"Home"} component={SignInUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/SignInUp.js
@@ -0,0 +1,136 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import { registration } from "../my-app/config/fireBaseMethods";
+
+//set matching password - maybe some verification steps?
+export default function App() {
+  const [firstName, setFirstName] = useState("");
+  const [familyName, setFamilyName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
+  const [signUp, setSignUp] = useState(true);
+  const [newUser, setNewUser] = useState({});
+
+  const handlePressSignIn = () => {
+    if (!email) {
+      Alert.alert("E-mail field is required");
+    }
+    if (!password) {
+      Alert.alert("Password field is required");
+    }
+    setUser({ email, password });
+    setEmail("");
+    setPassword("");
+  };
+
+  const handlePressSignUp = () => {
+    if (!email) {
+      Alert.alert("E-mail field is required");
+    }
+    if (!password) {
+      Alert.alert("Password field is required");
+    }
+    if (!firstName) {
+      Alert.alert("First Name field is required");
+    }
+    if (!familyName) {
+      Alert.alert("Family Name field is required");
+    }
+    setNewUser({ firstName, familyName, email, password });
+    registration(newUser);
+    setFirstName("");
+    setFamilyName("");
+    setEmail("");
+    setPassword("");
+    if (familyName && password && firstName && email) setSignUp(false);
+  };
+
+  const handleClick = () => {
+    signUp ? setSignUp(false) : setSignUp(true);
+  };
+
+  return signUp ? (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="First Name"
+        value={firstName}
+        onChangeText={(firstName) => {
+          setFirstName(firstName);
+        }}
+        style={styles.inputbox}
+      />
+
+      <TextInput
+        placeholder="Last Name"
+        value={familyName}
+        onChangeText={(familyName) => {
+          setFamilyName(familyName);
+        }}
+        style={styles.inputbox}
+      />
+
+      <TextInput
+        placeholder="E-mail"
+        value={email}
+        onChangeText={(email) => {
+          setEmail(email);
+        }}
+        style={styles.inputbox}
+      />
+
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={(password) => {
+          setPassword(password);
+        }}
+        style={styles.inputbox}
+        secureTextEntry={true}
+      />
+      <Button title="Sign-Up" onPress={handlePressSignUp} />
+      <Text onPress={handleClick}>I already have an account - Sign In</Text>
+    </View>
+  ) : (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="E-mail"
+        value={email}
+        onChangeText={(email) => {
+          setEmail(email);
+        }}
+        style={styles.inputbox}
+      />
+
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={(password) => {
+          setPassword(password);
+        }}
+        style={styles.inputbox}
+        secureTextEntry={true}
+      />
+
+      <Button title="Sign-In" onPress={handlePressSignIn} />
+      <Text onPress={handleClick}>I don't have an account yet - Sign-Up</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  inputbox: {
+    borderWidth: 2,
+    width: 100,
+    marginBottom: 10,
+  },
+});
