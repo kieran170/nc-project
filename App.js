4b91ddf5046d4c833e2ec40772a8291d19fbea41
ClaireAdele <claire.castanet@outlook.com>
2021-02-22 12:39:44 +0000

    setting up the app homepage

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from "react-navigation/native";
+import { createStackNavigator } from "react-navigation/stack";
+import SignInUp from "./components/SignInUp"
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Team!</Text>
-      <StatusBar style="auto" />
+      <SignInUp />
     </View>
   );
 }
--- my-app/config/firbase.js
+++ my-app/config/firbase.js
@@ -1,4 +1,8 @@
+import * as firebase from "firebase";
 import { database } from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyBXCFhIZHOlErk5jlVE1z3qGWQKc4lRcYg",
@@ -9,4 +13,9 @@ const firebaseConfig = {
     appId: "1:992843238778:web:543653378e3fb9659fdac3",
     measurementId: "G-5BYSEE5KYG",
     databaseURL: "https://nc-project-f7a5b.firebaseio.com"
-  };
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const auth = firebaseApp.auth();
+export const firestore = firebaseApp.firestore();
+
