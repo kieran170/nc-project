a20593c40fc2770c3dfcabe29d64375ef4da91b5
Ola-A-Aldroubi <ola.droubi.91@gmail.com>
2021-03-02 13:00:11 +0000

    creating contactList

--- App.js
+++ App.js
@@ -24,7 +24,8 @@ export default class App extends Component {
     const uid = firebase.auth().currentUser.uid;
     Promise.resolve(getUserInfo(uid)).then((userInfo) => {
       this.setState({ isLoggedIn: true, currentUser: { uid, ...userInfo } });
-    });
+    })
+  
   };
 
   handleLogOut = () => {
@@ -125,7 +126,7 @@ export default class App extends Component {
 
           <Stack.Screen name={"GroupChat"} component={GroupChat}></Stack.Screen>
           <Stack.Screen name={"ContactList"}>
-            {(props) => <ContactList user={this.state.currentUser} />}
+            {(props) => <ContactList  {...props} user={this.state.currentUser} />}
           </Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
--- components/ContactList.js
+++ components/ContactList.js
@@ -1,30 +1,70 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Alert, Button } from "react-native";
+import { View, Text, StyleSheet, Alert, Button ,FlatList} from "react-native";
 import { auth, firestore } from "../my-app/config/firebase";
 import ChatRoom from "../components/ChatRoom";
 
 export default function ContactList(props) {
+  
   const contacts = props.user.userData.contacts;
+  
+
   //list of contacts
   //we have the current user in props - we need to get the data for the contact user passed down
   const [userContacts, setContacts] = useState(contacts);
   const { navigation } = props;
+  const renderUser =(({item})=> {
+
+    const currentUserId= props.user.uid;
+    
+    return (
+    <View  style={styles.row}>
+        <Text>{item.name}</Text>
+        <ChatRoom
+          secondUser={{_id:item._id,firstName: item.firstName}}
+          currentUser={{    
+          firstName:props.user.userData.firstName,
+          _id:currentUserId,
+          }}
+        navigation={navigation}
+        ></ChatRoom>
+    </View>
+    );
+    });
 
   return (
     <View>
+
       <Text>Contacts</Text>
-      {secondUser.length ? (
-        <ChatRoom
-          secondUser={userContacts}
-          currentUser={{
-            firstName: props.route.params.user.name,
-            _id: props.route.params.user._id,
-          }}
-          navigation={navigation}
-        ></ChatRoom>
-      ) : (
-        <Text>Loading Contacts</Text>
-      )}
+
+       <FlatList data={contacts} renderItem={renderUser} keyExtractor={(item)=>item._id.toString()} />
+        
+       
+     
+
     </View>
   );
 }
+const styles=StyleSheet.create({
+  avatar:{
+      width:50,
+      height:50,
+      marginRight:10,
+  },
+  row:{
+      flexDirection: 'row',
+      padding:10,
+      alignItems:'center',
+      borderBottomColor:"#cacaca",
+      borderBottomWidth:1,
+  },
+  addUser:{
+      flexDirection: 'row',
+      padding:10,
+  },
+  input:{
+      backgroundColor: '#cacaca',
+      flex:1,
+      marginRight:10,
+      padding:10,
+  },
+});
