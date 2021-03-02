7096bef08802b46b019a693d36be930d696d327d
ClaireAdele <claire.castanet@outlook.com>
2021-03-02 10:37:15 +0000

    changes in firebase and stuff won't push

---
+++ my-app/config/firebase.js
@@ -0,0 +1,16 @@
+import { firebase } from "@firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyBXCFhIZHOlErk5jlVE1z3qGWQKc4lRcYg",
+    authDomain: "nc-project-f7a5b.firebaseapp.com",
+    projectId: "nc-project-f7a5b",
+    storageBucket: "nc-project-f7a5b.appspot.com",
+    messagingSenderId: "992843238778",
+    appId: "1:992843238778:web:543653378e3fb9659fdac3",
+    measurementId: "G-5BYSEE5KYG",
+    databaseURL: "https://nc-project-f7a5b.firebaseio.com"
+};
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const auth = firebaseApp.auth();
+export const firestore = firebaseApp.firestore();
